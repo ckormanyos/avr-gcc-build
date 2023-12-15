ab9859b5f535b69602b5612ce2115d22ac6d5d8f
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 05:53:00 +0100

    Really do use runner workspace

--- avr-gcc-build.sh
+++ avr-gcc-build.sh
@@ -8,13 +8,11 @@ mkdir -p gcc_build
 
 cd gcc_build
 
-mkdir -p usr
-
 wget --no-check-certificate https://libisl.sourceforge.io/isl-0.15.tar.bz2
 tar -xf isl-0.15.tar.bz2
 mkdir objdir-isl-0.15
 cd objdir-isl-0.15
-../isl-0.15/configure --prefix=/usr/local/isl-0.15 --build=x86_64-w64-mingw32 --host=x86_64-pc-linux --enable-static --disable-shared
+../isl-0.15/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/isl-0.15 --build=x86_64-w64-mingw32 --host=x86_64-pc-linux --enable-static --disable-shared
 
 make --jobs=4
 make install
