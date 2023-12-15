70602f61c4d9b37ebcca9137c1ed74f7205214c9
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 11:41:14 +0100

    Try to build GCC

--- avr-gcc-020-build-gcc.sh
+++ avr-gcc-020-build-gcc.sh
@@ -12,7 +12,7 @@ mkdir objdir-gcc-13.2.0-avr
 cd objdir-gcc-13.2.0-avr
 ../gcc-13.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=x86_64-unknown-linux-gnu --host=x86_64-w64-mingw32 --enable-languages=c,c++ --with-pkgversion='Built by ckormanyos/real-time-cpp' --disable-shared --enable-static --disable-libada --disable-libssp --disable-nls --enable-mingw-wildcard --with-gnu-as --with-dwarf2 --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
 
-# make --jobs=8
-# make install
+make --jobs=8
+make install
 
 exit
