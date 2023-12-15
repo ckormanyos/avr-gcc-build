bb7390e6424040337734d195bbdab64fb9d87d20
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 11:05:43 +0100

    Try to repair target/host configs

--- avr-gcc-002-build-gmp.sh
+++ avr-gcc-002-build-gmp.sh
@@ -10,7 +10,7 @@ cp ../gmp-6.3.0.tar.xz ./gmp-6.3.0.tar.xz
 tar -xf gmp-6.3.0.tar.xz
 mkdir objdir-gmp-6.3.0
 cd objdir-gmp-6.3.0
-../gmp-6.3.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --build=x86_64-w64-mingw32 --enable-static --disable-shared
+../gmp-6.3.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --enable-static --disable-shared
 
 make --jobs=8
 make install
