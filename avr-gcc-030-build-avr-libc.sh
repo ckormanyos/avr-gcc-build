d6538d7802a52abc9869a8d0f2637d91955aef7b
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 20:10:43 +0100

    Repair home build directory

--- avr-gcc-030-build-avr-libc.sh
+++ avr-gcc-030-build-avr-libc.sh
@@ -9,6 +9,12 @@ cd gcc_build
 cd objdir-gcc-13.2.0-avr
 ../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --build=`../gcc-13.2.0/config.guess` --host=avr
 
+PATH=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr/bin:"$PATH"
+export PATH
+
+CC=""
+export CC
+
 make --jobs=8
 make install
 
