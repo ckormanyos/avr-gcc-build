6b2ba7c23fd4d3cfe4ce9e53d29558fbe8efa3e8
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 09:10:22 +0100

    Retry ISL build

--- avr-gcc-005-build-isl.sh
+++ avr-gcc-005-build-isl.sh
@@ -10,7 +10,7 @@ wget --no-check-certificate https://libisl.sourceforge.io/isl-0.15.tar.bz2
 tar -xf isl-0.15.tar.bz2
 mkdir objdir-isl-0.15
 cd objdir-isl-0.15
-../isl-0.15/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/isl-0.15 --enable-static --disable-shared
+../isl-0.15/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/isl-0.15
 
 make --jobs=8
 make install
