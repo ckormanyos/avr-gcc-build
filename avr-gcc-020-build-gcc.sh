aaf6464a79e9b55a3d2eef3a6b365c5d00ce0133
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 13:08:28 +0100

    Add path for avr-gcc to its own build shell

--- avr-gcc-020-build-gcc.sh
+++ avr-gcc-020-build-gcc.sh
@@ -4,13 +4,33 @@
 #  Distributed under The Unlicense.
 #
 
+OPTS_GCC="
+ --target=avr
+ --enable-languages=c,c++
+ --disable-nls
+ --disable-libssp
+ --disable-libada
+ --with-dwarf2
+ --disable-shared
+ --enable-static
+ --enable-mingw-wildcard
+ --enable-plugin
+ --with-gnu-as
+"
+
 cd gcc_build
 
-wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz
-tar -xf gcc-12.2.0.tar.gz
-mkdir objdir-gcc-12.2.0-avr
-cd objdir-gcc-12.2.0-avr
-../gcc-12.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-12.2.0-avr --target=avr --build=x86_64-unknown-linux-gnu --host=x86_64-w64-mingw32 --enable-languages=c,c++ --with-pkgversion='Built by ckormanyos/real-time-cpp' --disable-shared --enable-static --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libada --disable-libssp --disable-libstdcxx-pch --disable-nls --enable-mingw-wildcard --with-gnu-as --with-dwarf2 --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
+wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz
+tar -xf gcc-13.2.0.tar.gz
+mkdir objdir-gcc-13.2.0-avr
+cd objdir-gcc-13.2.0-avr
+../gcc-13.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=x86_64-unknown-linux-gnu --host=x86_64-w64-mingw32 --enable-languages=c,c++ --with-pkgversion='Built by ckormanyos/real-time-cpp' "$OPTS_GCC" --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
+
+PATH=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr/bin:"$PATH"
+export PATH
+
+CC=""
+export CC
 
 make --jobs=8
 make install
