37c69d3af2cfed77cabd124e36211488eba423bc
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 09:07:47 +0100

    Try simplifying build

--- avr-gcc-002-build-gmp.sh
+++ avr-gcc-002-build-gmp.sh
@@ -6,11 +6,11 @@
 
 cd gcc_build
 
-cp ../gmp-6.3.0.tar.xz .
+cp ../gmp-6.3.0.tar.xz ./gmp-6.3.0.tar.xz
 tar -xf gmp-6.3.0.tar.xz
 mkdir objdir-gmp-6.3.0
 cd objdir-gmp-6.3.0
-../gmp-6.3.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --build=x86_64-w64-mingw32 --host=x86_64-unknown-linux-gnu --enable-static --disable-shared
+../gmp-6.3.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --enable-static --disable-shared
 
 make --jobs=8
 make install
