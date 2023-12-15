39d97a1d7ca6f07b9700ee38092981e45fd76c5b
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-15 11:11:55 +0100

    More host/target attempts

--- avr-gcc-005-build-isl.sh
+++ avr-gcc-005-build-isl.sh
@@ -10,7 +10,7 @@ wget --no-check-certificate https://libisl.sourceforge.io/isl-0.15.tar.bz2
 tar -xf isl-0.15.tar.bz2
 mkdir objdir-isl-0.15
 cd objdir-isl-0.15
-../isl-0.15/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/isl-0.15 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --enable-static --disable-shared --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
+../isl-0.15/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/isl-0.15 --build=x86_64-unknown-linux-gnu --target=x86_64-w64-mingw32 --enable-static --disable-shared --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
 
 make --jobs=8
 make install
