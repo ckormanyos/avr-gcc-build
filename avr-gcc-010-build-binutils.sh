c149d0da8827b6d6e7d5a4abaa7dcdca2c57329f
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-16 03:59:13 +0100

    Actually avr-gcc is not being built entirely

--- avr-gcc-010-build-binutils.sh
+++ avr-gcc-010-build-binutils.sh
@@ -10,7 +10,7 @@ wget --no-check-certificate https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.b
 tar -xf binutils-2.41.tar.bz2
 mkdir objdir-binutils-2.41-avr-gcc-13.2.0
 cd objdir-binutils-2.41-avr-gcc-13.2.0
-../binutils-2.41/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=x86_64-unknown-linux-gnu --host=x86_64-w64-mingw32 --enable-languages=c,c++ --enable-static --disable-shared --disable-werror --with-pkgversion='Built by ckormanyos/real-time-cpp' --disable-libada --disable-libssp --disable-nls --enable-mingw-wildcard --with-gnu-as --with-dwarf2 --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
+../binutils-2.41/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=x86_64-unknown-linux-gnu --host=x86_64-w64-mingw32  --with-pkgversion='Built by ckormanyos/real-time-cpp' --disable-shared --enable-static --disable-nls --disable-werror --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
 
 make --jobs=8
 make install
--- avr-gcc-030-build-avr-libc.sh
+++ avr-gcc-030-build-avr-libc.sh
@@ -14,7 +14,7 @@ cd gcc_build/avr-libc3
 ./bootstrap
 
 cd ../cd objdir-gcc-13.2.0-avr
-../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --host=x86_64-w64-mingw32 --build=`../gcc-13.2.0/config.guess`
+../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --host=avr
 
 make --jobs=8
 make install
