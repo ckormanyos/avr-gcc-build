c23c6c14a7212ec1246be6ee7b260d6e12655de6
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-16 03:37:34 +0100

    Streamline avr-libc build and retry it

--- avr-gcc-030-build-avr-libc.sh
+++ avr-gcc-030-build-avr-libc.sh
@@ -10,14 +10,11 @@ export PATH
 CC=""
 export CC
 
-cd gcc_build
-
-cd avr-libc3
+cd gcc_build/avr-libc3
 ./bootstrap
 
-cd ..
-cd objdir-gcc-13.2.0-avr
-../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --build=`../gcc-13.2.0/config.guess` --host=avr
+cd ../cd objdir-gcc-13.2.0-avr
+../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --host=x86_64-w64-mingw32 --build=`../gcc-13.2.0/config.guess`
 
 make --jobs=8
 make install
