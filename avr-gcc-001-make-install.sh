b1a2e6eb81a0adc3544f17a9cc720a8b85727861
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-17 18:43:22 +0100

    Simplify native linux build

--- avr-gcc-001-make-install.sh
+++ avr-gcc-001-make-install.sh
@@ -63,4 +63,21 @@ cd objdir-gcc-13.2.0-avr
 make --jobs=8
 make install
 
+
+
+PATH=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr/bin:"$PATH"
+export PATH
+
+CC=""
+export CC
+
+cd gcc_build/avr-libc3
+./bootstrap
+
+cd ../cd objdir-gcc-13.2.0-avr
+../avr-libc3/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --host=avr
+
+make --jobs=8
+make install
+
 exit 0
