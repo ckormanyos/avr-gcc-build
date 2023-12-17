bda1ee72809accd5b60d5d0de9e9592cfef90fdf
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-17 18:35:22 +0100

    Simplify build params

--- avr-gcc-001-make-install.sh
+++ avr-gcc-001-make-install.sh
@@ -58,7 +58,7 @@ wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.ta
 tar -xf gcc-13.2.0.tar.gz
 mkdir objdir-gcc-13.2.0-avr
 cd objdir-gcc-13.2.0-avr
-../gcc-13.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --with-pkgversion='Built by ckormanyos/real-time-cpp' --enable-languages=c,c++ --disable-nls --disable-libssp --disable-libada --with-dwarf2 --disable-shared --enable-static --enable-mingw-wildcard --enable-plugin --with-gnu-as --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
+../gcc-13.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --with-pkgversion='Built by ckormanyos/real-time-cpp' --enable-languages=c,c++,lto --disable-nls --disable-libssp --disable-libada --with-dwarf2 --disable-shared --enable-static --enable-mingw-wildcard --enable-plugin --with-gnu-as --with-gnu-ld --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
 
 make --jobs=8
 make install
