6d073b52334f2442f7e63aafb83ec07c4518b703
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-17 17:59:15 +0100

    no message

--- avr-gcc-020-build-gcc.sh
+++ avr-gcc-020-build-gcc.sh
@@ -6,13 +6,13 @@
 
 cd gcc_build
 
-wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.gz
-tar -xf gcc-12.1.0.tar.gz
-mkdir objdir-gcc-12.1.0-avr
-cd objdir-gcc-12.1.0-avr
-../gcc-12.1.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-12.1.0-avr --target=avr --host=x86_64-w64-mingw32 --build=`../gcc-12.1.0/config.guess` --with-pkgversion='Built by ckormanyos/real-time-cpp' --enable-languages=c,c++ --disable-nls --disable-libssp --disable-libada --with-dwarf2 --disable-shared --enable-static --enable-mingw-wildcard --enable-plugin --with-gnu-as --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
+wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.gz
+tar -xf gcc-13.2.0.tar.gz
+mkdir objdir-gcc-13.2.0-avr
+cd objdir-gcc-13.2.0-avr
+../gcc-13.2.0/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --host=x86_64-w64-mingw32 --build=`../gcc-13.2.0/config.guess` --with-pkgversion='Built by ckormanyos/real-time-cpp' --enable-languages=c,c++ --disable-nls --disable-libssp --disable-libada --with-dwarf2 --disable-shared --enable-static --enable-mingw-wildcard --enable-plugin --with-gnu-as --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0 --with-mpfr=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpfr-4.2.1 --with-mpc=/home/runner/work/avr-gcc-build/avr-gcc-build/local/mpc-1.2.1
 
-PATH=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-12.1.0-avr/bin:"$PATH"
+PATH=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr/bin:"$PATH"
 export PATH
 
 CC=""
