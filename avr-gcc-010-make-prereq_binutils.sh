4d39ffc7ca0a5d44ce29e9404549e7502bb7ed52
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-18 05:35:18 +0100

    Repair (or try to) binutils build

--- avr-gcc-010-make-prereq_binutils.sh
+++ avr-gcc-010-make-prereq_binutils.sh
@@ -19,6 +19,8 @@ cd objdir-gmp-6.3.0
 make --jobs=6
 make install
 
+
+
 wget --no-check-certificate https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.bz2
 tar -xf mpfr-4.2.1.tar.bz2
 mkdir objdir-mpfr-4.2.1
@@ -27,6 +29,8 @@ cd objdir-mpfr-4.2.1
 
 make --jobs=6
 make install
+
+
 
 wget --no-check-certificate https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
 tar -xf mpc-1.2.1.tar.gz
@@ -37,11 +41,14 @@ cd objdir-mpc-1.2.1
 make --jobs=6
 make install
 
+
+
+
 wget --no-check-certificate https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.bz2
 tar -xf binutils-2.41.tar.bz2
 mkdir objdir-binutils-2.41-avr-gcc-13.2.0
 cd objdir-binutils-2.41-avr-gcc-13.2.0
-../binutils-2.41/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=$HOST_NAME --host=$HOST_NAME --disable-shared --enable-static --disable-libada --disable-libssp --disable-nls --disable-quadmath --disable-decimal_float --enable-mingw-wildcard --with-dwarf2 --enable-plugin --with-gnu-as --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
+../binutils-2.41/configure --prefix=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gcc-13.2.0-avr --target=avr --build=$HOST_NAME --host=$HOST_NAME --disable-shared --enable-static --disable-libada --disable-libssp --disable-nls --disable-quadmath --disable-decimal-float --enable-mingw-wildcard --with-dwarf2 --enable-plugin --with-gnu-as --with-gmp=/home/runner/work/avr-gcc-build/avr-gcc-build/local/gmp-6.3.0
 
 make --jobs=6
 make install
