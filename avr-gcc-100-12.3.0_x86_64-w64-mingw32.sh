29e0cf6ae38765930ac0493844237af395ab19b3
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-28 17:05:49 +0100

    Try build binutils and then see next steps

--- avr-gcc-100-12.3.0_x86_64-w64-mingw32.sh
+++ avr-gcc-100-12.3.0_x86_64-w64-mingw32.sh
@@ -51,12 +51,12 @@ cd $SCRIPT_DIR/gcc_build
 echo 'get tar-balls'
 wget --no-check-certificate https://github.com/facebook/zstd/releases/download/v1.5.5/zstd-1.5.5.tar.gz
 wget --no-check-certificate https://ftp.gnu.org/gnu/libiconv/libiconv-1.17.tar.gz
-#wget --no-check-certificate https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
-#wget --no-check-certificate https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.xz
-#wget --no-check-certificate https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
-#wget --no-check-certificate https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2
-#wget --no-check-certificate https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
-#wget --no-check-certificate https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz
+wget --no-check-certificate https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
+wget --no-check-certificate https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.xz
+wget --no-check-certificate https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
+wget --no-check-certificate https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2
+wget --no-check-certificate https://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz
+wget --no-check-certificate https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz
 #wget --no-check-certificate https://ftp.gnu.org/gnu/gcc/gcc-12.3.0/gcc-12.3.0.tar.xz
 
 
@@ -76,5 +76,65 @@ tar -xf libiconv-1.17.tar.gz
 mkdir objdir-libiconv-1.17
 cd objdir-libiconv-1.17
 ../libiconv-1.17/configure --prefix=$SCRIPT_DIR/local/libiconv-1.17 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build gmp'
+tar -xf gmp-6.3.0.tar.xz
+mkdir objdir-gmp-6.3.0
+cd objdir-gmp-6.3.0
+../gmp-6.3.0/configure --prefix=$SCRIPT_DIR/local/gmp-6.3.0 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build mpfr'
+tar -xf mpfr-4.2.1.tar.xz
+mkdir objdir-mpfr-4.2.1
+cd objdir-mpfr-4.2.1
+../mpfr-4.2.1/configure --prefix=$SCRIPT_DIR/local/mpfr-4.2.1 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared --with-gmp=$SCRIPT_DIR/local/gmp-6.3.0
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build mpc'
+tar -xf mpc-1.3.1.tar.gz
+mkdir objdir-mpc-1.3.1
+cd objdir-mpc-1.3.1
+../mpc-1.3.1/configure --prefix=$SCRIPT_DIR/local/mpc-1.3.1 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared --with-gmp=$SCRIPT_DIR/local/gmp-6.3.0 --with-mpfr=$SCRIPT_DIR/local/mpfr-4.2.1
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build isl'
+tar -xjf isl-0.15.tar.bz2
+mkdir objdir-isl-0.15
+cd objdir-isl-0.15
+../isl-0.15/configure --prefix=$SCRIPT_DIR/local/isl-0.15 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared --with-gmp-prefix=$SCRIPT_DIR/local/gmp-6.3.0
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build cloog'
+tar -xf cloog-0.18.1.tar.gz
+mkdir objdir-cloog-0.18.1
+cd objdir-cloog-0.18.1
+../cloog-0.18.1/configure --prefix=$SCRIPT_DIR/local/cloog-0.18.1 --build=x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --enable-static --disable-shared --with-isl=$SCRIPT_DIR/local/isl-0.15 --with-gmp=$SCRIPT_DIR/local/gmp-6.3.0
+make --jobs=6
+make install
+
+
+cd $SCRIPT_DIR/gcc_build
+echo 'build binutils'
+tar -xf binutils-2.41.tar.xz
+mkdir objdir-binutils-2.41-avr-gcc-12.3.0
+cd objdir-binutils-2.41-avr-gcc-12.3.0
+../binutils-2.41/configure --prefix=$SCRIPT_DIR/local/gcc-12.3.0-avr --target=avr --enable-languages=c,c++ --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --with-pkgversion='Built by ckormanyos/real-time-cpp' --enable-static --disable-shared --disable-libada --disable-libssp --disable-nls --enable-mingw-wildcard --with-gnu-as --with-dwarf2 --with-isl=$SCRIPT_DIR/local/isl-0.15 --with-cloog=$SCRIPT_DIR/local/cloog-0.18.1 --with-gmp=$SCRIPT_DIR/local/gmp-6.3.0 --with-mpfr=$SCRIPT_DIR/local/mpfr-4.2.1 --with-mpc=$SCRIPT_DIR/local/mpc-1.3.1 --with-libiconv-prefix=$SCRIPT_DIR/local/libiconv-1.17 --with-zstd=$SCRIPT_DIR/local/zstd-1.5.5/lib --disable-werror
 make --jobs=6
 make install
