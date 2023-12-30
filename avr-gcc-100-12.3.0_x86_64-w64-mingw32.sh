506023ef123299eb6fe6c165c9d8363c0148a36a
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-12-30 11:34:57 +0100

    Correct typo on tar command

--- avr-gcc-100-12.3.0_x86_64-w64-mingw32.sh
+++ avr-gcc-100-12.3.0_x86_64-w64-mingw32.sh
@@ -199,10 +199,10 @@ echo
 
 cd $SCRIPT_DIR/gcc_build
 echo 'unpack and bootstrap avr-libc'
-tar -xf avr-libc-2_1_0-release.tar.gz ./avr-libc-2_1_0-release
-rm -f avr-libc-2_1_0-release/tests/simulate/time/aux.c
+tar -xf avr-libc-2_1_0-release.tar.gz
 mv avr-libc-avr-libc-2_1_0-release avr-libc-2_1_0-release
 ls -la
+rm -f avr-libc-2_1_0-release/tests/simulate/time/aux.c
 cd avr-libc-2_1_0-release
 ./bootstrap
 result_bootstrap=$?
